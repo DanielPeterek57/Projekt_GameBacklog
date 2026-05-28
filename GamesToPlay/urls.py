d99a6cc7ad6259aa09603ddaae84ceeb81b6d3c6
Daniel Peterek <D23843@oa-opava.cz>
2026-05-28 17:57:40 +0200

    Záloha změn před pullem

--- GamesToPlay/urls.py
+++ GamesToPlay/urls.py
@@ -5,7 +5,7 @@ from Hry import views  # Čistý import views z aplikace Hry
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aktualizuj-hltb-hry/', views.aktualizuj_hltb_hry, name='aktualizuj_hltb_hry'),
-git init
+
     # Hlavní stránka tvého herního backlogu
     path('moje-hry/', views.seznam_her, name='seznam_her'),
     path('import/', views.nacist_ze_steam, name='import_her'),
